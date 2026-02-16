3af26222c939666d5570086e3340cac05b14a9ff
3nd0rf1n <saylen2006@gmail.com>
2026-02-16 16:21:07 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,32 @@
+const CACHE_NAME = 'sim-tracker-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+// Install SW and cache files
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('v1').then((c) => c.addAll(['./index.html'])));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
 });
+
+// Activate SW and clean old caches
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch from cache first, fallback to network
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
